f0f1ec08d6725ed6c44600e6bf31a8ae598b724e
tayjaf <tayyabx@gmail.com>
2023-01-03 14:04:13 -0500

    update

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -7,14 +7,27 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s is your favorite color?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(children: [
+          Text('The question!'),
+          ElevatedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+          ElevatedButton(child: Text('Answer 2'), onPressed: answerQuestion),
+          ElevatedButton(child: Text('Answer 3'), onPressed: answerQuestion),
+        ]),
       ),
     );
   }
